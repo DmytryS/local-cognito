859b26d7914b0eb42fa43b629489ce3275aae14e
Dmytro Shvaika <dmytro.shvaika@legatics.com>
2021-05-28 03:31:35 +0300

    WIP

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
-const crypto = require( 'crypto' );
-const { inspect } = require( 'util' );
-const { send, json } = require( 'micro' );
-const {
-	router,
-	options,
-	post,
-	get,
-} = require( 'micro-fork' );
+import 'dotenv/config.js'
+import crypto from 'crypto'
+import { inspect } from 'util'
+import http from 'http'
+import express from 'express'
+import bodyParser from 'body-parser'
+
+const { HOST, PORT } = process.env
 
 const KMSKey = Buffer.from( 'w+YQ0eVUHNzQ2zp/u8Ip1aRsaJQ2sgWzAS5umnXA7JA=', 'base64' );
-const iv = Buffer.from('axYxUkFIAe/RMVze4Wkcig==', 'base64');
+const iv = Buffer.from( 'axYxUkFIAe/RMVze4Wkcig==', 'base64' );
 const KMSAlg = 'aes-256-cbc'
 
 const storage = {
@@ -110,17 +109,17 @@ const uuid = placeholder =>
 		: ( [ 1e7 ] + -1e3 + -4e3 + -8e3 + -1e11 ).replace( /[018]/g, uuid );
 
 const setHeaders = ( req, res, headers = {} ) => {
-	res.setHeader( 'access-control-allow-origin', '*' );
+	res.set( 'access-control-allow-origin', '*' );
 	if ( req.headers['access-control-request-headers'] ) {
-		res.setHeader( 'access-control-allow-headers', req.headers['access-control-request-headers'] );
+		res.set( 'access-control-allow-headers', req.headers['access-control-request-headers'] );
 	}
-	res.setHeader( 'access-control-expose-headers', 'x-amzn-RequestId,x-amzn-ErrorType,x-amzn-ErrorMessage,Date' );
-	res.setHeader( 'access-control-allow-methods', req.headers['access-control-allow-methods'] || 'GET, PUT, POST, DELETE, HEAD, OPTIONS' );
-	res.setHeader( 'access-control-max-age', '172800' );
-	res.setHeader( 'date', new Date().toUTCString() );
-	res.setHeader( 'x-amzn-requestid', uuid() );
+	res.set( 'access-control-expose-headers', 'x-amzn-RequestId,x-amzn-ErrorType,x-amzn-ErrorMessage,Date' );
+	res.set( 'access-control-allow-methods', req.headers['access-control-allow-methods'] || 'GET, PUT, POST, DELETE, HEAD, OPTIONS' );
+	res.set( 'access-control-max-age', '172800' );
+	res.set( 'date', new Date().toUTCString() );
+	res.set( 'x-amzn-requestid', uuid() );
 	Object.entries( headers, ( [ key, value ] ) => {
-		res.setHeader( key, value );
+		res.set( key, value );
 	} );
 };
 
@@ -130,14 +129,20 @@ const renameProp = ( prev, next, object = {} ) => ( {
 	[prev]: undefined,
 } );
 
-module.exports = router()(
-	get( '/', ( req, res ) => send( res, 200, { message: 'Service is running' } ) ),
-	options( '/', ( req, res ) => {
+const app = express()
+
+app.use( bodyParser.urlencoded( { extended: true } ) )
+app.use( bodyParser.json() )
+
+app.route( '/' )
+	.get( ( req, res ) => res.status( 200 ).send( { message: 'Service is running' } ) )
+	.options( ( req, res ) => {
 		setHeaders( req, res );
-		send( res, 200 );
-	} ),
-	post( '/', async ( req, res ) => {
-		const body = await json( req );
+		res.sendStatus( 200 )
+	} )
+	.post( async ( req, res ) => {
+		const body = req.body;
+		console.log( '---BODY::', body )
 		console.log( req.headers['x-amz-target'], inspect( body, { depth: 12 } ) );
 
 		setHeaders( req, res, {
@@ -148,7 +153,7 @@ module.exports = router()(
 		if ( req.headers['x-amz-target'] === 'AWSCognitoIdentityService.GetCredentialsForIdentity' ) {
 			const expiration = new Date();
 			expiration.setTime( expiration.getTime() + ( 60 * 60 * 1000 ) );
-			send( res, 200, {
+			res.status( 200 ).json( {
 				Credentials: {
 					AccessKeyId: 'not-needed',
 					Expiration: expiration.toISOString(),
@@ -162,7 +167,7 @@ module.exports = router()(
 
 		// Get new ID.
 		if ( req.headers['x-amz-target'] === 'AWSCognitoIdentityService.GetId' ) {
-			send( res, 200, {
+			res.status( 200 ).json( {
 				IdentityId: `us-east-1:${uuid()}`,
 			} );
 			return;
@@ -170,7 +175,7 @@ module.exports = router()(
 
 		// List Users
 		if ( req.headers['x-amz-target'] === 'AWSCognitoIdentityProviderService.ListUsers' ) {
-			send( res, 200, {
+			res.status( 200 ).json( {
 				Users: storage.users.list( body.Filter ),
 			} );
 			return;
@@ -178,8 +183,10 @@ module.exports = router()(
 
 		// Admin Delete User
 		if ( req.headers['x-amz-target'] === 'AWSCognitoIdentityProviderService.AdminDeleteUser' ) {
+			console.log( '-----BEFORE' )
 			storage.users.del( body.Username );
-			send( res, 200 );
+			console.log( '-----AFTER' )
+			res.status( 200 ).json( {} )
 			return;
 		}
 
@@ -187,7 +194,7 @@ module.exports = router()(
 		if ( req.headers['x-amz-target'] === 'AWSCognitoIdentityProviderService.AdminUpdateUserAttributes' ) {
 			const User = storage.users.get( body.Username );
 			if ( ! User ) {
-				send( res, 404 );
+				res.sendStatus( 404 )
 				return
 			}
 
@@ -204,7 +211,7 @@ module.exports = router()(
 				userAttribute.Value = Value;
 			}
 
-			send( res, 200, { User } );
+			res.status( 200 ).json( { User } );
 			return;
 		}
 
@@ -212,7 +219,9 @@ module.exports = router()(
 		if ( req.headers['x-amz-target'] === 'AWSCognitoIdentityProviderService.AdminCreateUser' ) {
 			const userUUID = uuid();
 			const addAttribute = ( Name, defaultValue ) => {
-				let Value = ( body.UserAttributes || [] ).filter( ( { Name: _name } ) => _name === Name ).map( ( { Value } ) => Value )[0];
+				let Value = ( body.UserAttributes || [] )
+					.filter( ( { Name: _name } ) => _name === Name )
+					.map( ( { Value } ) => Value )[0];
 				if ( ! defaultValue && ! Value ) {
 					return [];
 				}
@@ -252,7 +261,7 @@ module.exports = router()(
 
 			storage.users.add( User );
 
-			send( res, 200, { User } );
+			res.status( 200 ).json( { User } );
 			return
 		}
 
@@ -261,14 +270,14 @@ module.exports = router()(
 			const token = uuid();
 			const User = storage.users.auth( body.AuthParameters.USERNAME, body.AuthParameters.PASSWORD );
 			if ( ! User ) {
-				send( res, 401 );
+				res.sendStatus( 401 );
 				return;
 			}
 
 			storage.tokens[token] = User;
 			storage.links.get( User ).push( () => delete storage.tokens[token] );
 
-			send( res, 200, {
+			res.status( 200 ).json( {
 				'ChallengeParameters': {},
 				'AuthenticationResult': {
 					'AccessToken': token,
@@ -285,11 +294,11 @@ module.exports = router()(
 		if ( req.headers['x-amz-target'] === 'AWSCognitoIdentityProviderService.GetUser' ) {
 			let User = storage.tokens[body.AccessToken];
 			if ( ! User ) {
-				send( res, 401 );
+				res.sendStatus( 401 );
 				return
 			}
 
-			send( res, 200, renameProp( 'Attributes', 'UserAttributes', User ) );
+			res.status( 200 ).json( renameProp( 'Attributes', 'UserAttributes', User ) );
 			return;
 		}
 
@@ -297,7 +306,7 @@ module.exports = router()(
 		if ( req.headers['x-amz-target'] === 'AWSCognitoIdentityProviderService.AdminSetUserPassword' ) {
 			const User = storage.users.get( body.Username );
 			if ( ! User ) {
-				send( res, 404 );
+				res.sendStatus( 404 );
 				return;
 			}
 
@@ -305,38 +314,38 @@ module.exports = router()(
 			storage.auth[key] = User;
 			storage.links.get( User ).push( () => delete storage.auth[key] );
 
-			send( res, 200, renameProp( 'Attributes', 'UserAttributes', User ) );
+			res.status( 200 ).json( renameProp( 'Attributes', 'UserAttributes', User ) );
 			return;
 		}
 
 		// Admin Get User.
 		if ( req.headers['x-amz-target'] === 'AWSCognitoIdentityProviderService.AdminGetUser' ) {
-			send( res, 200, renameProp( 'Attributes', 'UserAttributes', storage.users.get( body.Username ) ) );
+			res.status( 200 ).json( renameProp( 'Attributes', 'UserAttributes', storage.users.get( body.Username ) ) );
 			return;
 		}
 
 		if ( req.headers['x-amz-target'] === 'AWSCognitoIdentityProviderService.AdminAddUserToGroup' ) {
 			const User = storage.users.get( body.Username );
 			if ( ! User ) {
-				send( res, 404 );
+				res.sendStatus( 404 )
 				return;
 			}
 
 			const Groups = User.groups = User.groups || new Set();
 			Groups.add( body.GroupName );
-			send( res, 200 );
+			res.sendStatus( 200 )
 			return;
 		}
 
 		if ( req.headers['x-amz-target'] === 'AWSCognitoIdentityProviderService.AdminListGroupsForUser' ) {
 			const User = storage.users.get( body.Username );
 			if ( ! User ) {
-				send( res, 404 );
+				res.sendStatus( 404 )
 				return;
 			}
 
 			const Groups = User.groups = User.groups || new Set();
-			send( res, 200, {
+			res.status( 200 ).json( {
 				Groups: [ ...Groups ].map( GroupName => ( {
 					GroupName,
 					UserPoolId: body.UserPoolId,
@@ -349,8 +358,8 @@ module.exports = router()(
 
 		if ( req.headers['x-amz-target'] === 'TrentService.Decrypt' ) {
 			const { CiphertextBlob, KeyId = '349a3879-11a2-445d-95d0-0ba526ae0a1c' } = body
-			if (CiphertextBlob === 'AQIDAHjfsMkryEoNjhnww46u7deWahqtig9q5lAzCNI77t8sDAHgysjgmApFvD1iyi4SzXtVAAAAfjB8BgkqhkiG9w0BBwagbzBtAgEAMGgGCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQMQ3ghIFmJdFE2hNp+AgEQgDsuPR8MN0gIbpBeI6pzagZvLg2Y7J6qrxaAb70mVM1qGSO7FmrIFnsMAYjg6M/7NeptSjGdjjGVtMqoug==') {
-				send( res, 200, {
+			if ( CiphertextBlob === 'AQIDAHjfsMkryEoNjhnww46u7deWahqtig9q5lAzCNI77t8sDAHgysjgmApFvD1iyi4SzXtVAAAAfjB8BgkqhkiG9w0BBwagbzBtAgEAMGgGCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQMQ3ghIFmJdFE2hNp+AgEQgDsuPR8MN0gIbpBeI6pzagZvLg2Y7J6qrxaAb70mVM1qGSO7FmrIFnsMAYjg6M/7NeptSjGdjjGVtMqoug==' ) {
+				res.status( 200 ).json( {
 					KeyId: `arn:aws:kms:eu-west-1:123456789010:key/${KeyId}`,
 					Plaintext: 'wu5MqCDkKzPfAQoZAfDuBbodlC7Brmwq/2hj48X76AI=',
 					EncryptionAlgorithm: 'SYMMETRIC_DEFAULT',
@@ -358,29 +367,29 @@ module.exports = router()(
 				return
 			}
 
-                        if (CiphertextBlob === 'AQIDAHgUKAdXUQ893Kgcn5VWOTowFgXi5CbaL96g1wNgcEgkQgEQnxgmQLpWgJquZ6zDeeOMAAAAfjB8BgkqhkiG9w0BBwagbzBtAgEAMGgGCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQMhs6KB1RoxyND+XiXAgEQgDst07n+He+VKUkw8BJ5sOCutpYwsgz+Bqtd0usUdqXZppRVAW9kOR21nj/heeKnufJu1tNLhmpaSgL94g==') {
-                                send( res, 200, {
-                                        KeyId: `arn:aws:kms:eu-west-1:123456789010:key/${KeyId}`,
-                                        Plaintext: 'AdwsxSZHDzFpu4ZUTY9LIo23NW9MRpN6RIyZhEaxB2c=',
-                                        EncryptionAlgorithm: 'SYMMETRIC_DEFAULT',
-                                } );
-                                return
-                        }
+			if ( CiphertextBlob === 'AQIDAHgUKAdXUQ893Kgcn5VWOTowFgXi5CbaL96g1wNgcEgkQgEQnxgmQLpWgJquZ6zDeeOMAAAAfjB8BgkqhkiG9w0BBwagbzBtAgEAMGgGCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQMhs6KB1RoxyND+XiXAgEQgDst07n+He+VKUkw8BJ5sOCutpYwsgz+Bqtd0usUdqXZppRVAW9kOR21nj/heeKnufJu1tNLhmpaSgL94g==' ) {
+				res.status( 200 ).json( {
+					KeyId: `arn:aws:kms:eu-west-1:123456789010:key/${KeyId}`,
+					Plaintext: 'AdwsxSZHDzFpu4ZUTY9LIo23NW9MRpN6RIyZhEaxB2c=',
+					EncryptionAlgorithm: 'SYMMETRIC_DEFAULT',
+				} );
+				return
+			}
 
-                        if (CiphertextBlob === 'AQIDAHgUKAdXUQ893Kgcn5VWOTowFgXi5CbaL96g1wNgcEgkQgGHhACaJFdMEJumJ0BmaK7ZAAAAfjB8BgkqhkiG9w0BBwagbzBtAgEAMGgGCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQMPy+1eF3v8QUZ5YlZAgEQgDuHVvFF+tNeLdwY/3roXfcO4IX+RPHS4eSkwNr6WD9JIcxqHcxuj8eoU2g2fNTXgCCNBO+wIlIGpZeuvg==') {
-                                send( res, 200, {
-                                        KeyId: `arn:aws:kms:eu-west-1:123456789010:key/${KeyId}`,
-                                        Plaintext: 'V+jITuFQmSyZqAvwS1p77oe7kZxlMWM3om4/l0o+ADY=',
-                                        EncryptionAlgorithm: 'SYMMETRIC_DEFAULT',
-                                } );
-                                return
-                        }
+			if ( CiphertextBlob === 'AQIDAHgUKAdXUQ893Kgcn5VWOTowFgXi5CbaL96g1wNgcEgkQgGHhACaJFdMEJumJ0BmaK7ZAAAAfjB8BgkqhkiG9w0BBwagbzBtAgEAMGgGCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQMPy+1eF3v8QUZ5YlZAgEQgDuHVvFF+tNeLdwY/3roXfcO4IX+RPHS4eSkwNr6WD9JIcxqHcxuj8eoU2g2fNTXgCCNBO+wIlIGpZeuvg==' ) {
+				res.status( 200 ).json( {
+					KeyId: `arn:aws:kms:eu-west-1:123456789010:key/${KeyId}`,
+					Plaintext: 'V+jITuFQmSyZqAvwS1p77oe7kZxlMWM3om4/l0o+ADY=',
+					EncryptionAlgorithm: 'SYMMETRIC_DEFAULT',
+				} );
+				return
+			}
 
-                        const decipher = crypto.createDecipheriv(KMSAlg, KMSKey, iv);
-			const decrypted = Buffer.concat([ decipher.update(Buffer.from(CiphertextBlob, 'base64')), decipher.final() ]);
-			send( res, 200, {
+			const decipher = crypto.createDecipheriv( KMSAlg, KMSKey, iv );
+			const decrypted = Buffer.concat( [ decipher.update( Buffer.from( CiphertextBlob, 'base64' ) ), decipher.final() ] );
+			res.status( 200 ).json( {
 				KeyId: `arn:aws:kms:eu-west-1:123456789010:key/${KeyId}`,
-				Plaintext: decrypted.toString('base64'),
+				Plaintext: decrypted.toString( 'base64' ),
 				EncryptionAlgorithm: 'SYMMETRIC_DEFAULT',
 			} );
 			return;
@@ -388,10 +397,10 @@ module.exports = router()(
 
 		if ( req.headers['x-amz-target'] === 'TrentService.Encrypt' ) {
 			const { Plaintext, KeyId = '349a3879-11a2-445d-95d0-0ba526ae0a1c' } = body
-			const cipher = crypto.createCipheriv(KMSAlg, KMSKey, iv);
-			const encrypted = Buffer.concat([ cipher.update(Buffer.from(Plaintext, 'base64')), cipher.final() ]);
-			send( res, 200, {
-				CiphertextBlob: encrypted.toString('base64'),
+			const cipher = crypto.createCipheriv( KMSAlg, KMSKey, iv );
+			const encrypted = Buffer.concat( [ cipher.update( Buffer.from( Plaintext, 'base64' ) ), cipher.final() ] );
+			res.status( 200 ).json( {
+				CiphertextBlob: encrypted.toString( 'base64' ),
 				KeyId: `arn:aws:kms:eu-west-1:123456789010:key/${KeyId}`,
 				EncryptionAlgorithm: 'SYMMETRIC_DEFAULT',
 			} );
@@ -399,13 +408,14 @@ module.exports = router()(
 		}
 
 		if ( req.headers['x-amz-target'] === 'TrentService.GenerateDataKey' ) {
+			// eslint-disable-next-line
 			const { KeyId = '349a3879-11a2-445d-95d0-0ba526ae0a1c', KeySpec = 'AES_256' } = body
-			const random = crypto.randomBytes(32);
-			const cipher = crypto.createCipheriv(KMSAlg, KMSKey, iv);
-			const encrypted = Buffer.concat([ cipher.update(random), cipher.final() ]);
-			send( res, 200, {
-				CiphertextBlob: encrypted.toString('base64'),
-				Plaintext: random.toString('base64'),
+			const random = crypto.randomBytes( 32 );
+			const cipher = crypto.createCipheriv( KMSAlg, KMSKey, iv );
+			const encrypted = Buffer.concat( [ cipher.update( random ), cipher.final() ] );
+			res.status( 200 ).json( {
+				CiphertextBlob: encrypted.toString( 'base64' ),
+				Plaintext: random.toString( 'base64' ),
 				KeyId: `arn:aws:kms:eu-west-1:123456789010:key/${KeyId}`,
 			} );
 			return;
@@ -413,6 +423,13 @@ module.exports = router()(
 
 		console.error( req.headers['x-amz-target'] )
 		process.exit( 123 )
-		send( res, 500 );
-	} ),
-);
+		res.sendStatus( 500 )
+	} )
+
+const httpServer = http.createServer( app )
+
+const onReady = () => {
+	console.log( `🚀 Server ready at http://${HOST}:${PORT}` )
+}
+
+httpServer.listen( PORT, HOST, onReady )
